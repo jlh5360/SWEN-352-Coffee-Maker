a33ffd802928a8472a9afbe026ead75fa42597d2
cowdrinksmilk <mda6282@rit.edu>
2025-06-05 01:24:18 -0400

    mary-almazan: Task 1: create a unit test for recipe finished, i made a sample test

---
+++ src/test/java/coffeemaker/RecipeTest.java
@@ -0,0 +1,27 @@
+package coffeemaker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import coffeemaker.domain.Recipe;
+
+public class RecipeTest {
+
+    // unused for task 1 for now, delete this comment later
+    @SuppressWarnings("unused")
+    private Recipe recipe;
+
+    // 0 code coverage - Task 1
+     @BeforeEach
+    public void setUp() {
+        recipe = new Recipe();
+    }
+
+    @Test
+    @DisplayName("Sample test: Task 1")
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}

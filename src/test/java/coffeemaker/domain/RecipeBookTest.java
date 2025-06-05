0d9d4c5b2de6ac4ab2abf8e8745cd8a1c831136a
mpeck2023 <121977879+mpeck2023@users.noreply.github.com>
2025-06-05 10:49:08 -0400

    task 1 and 2 done, working on task three

--- src/test/java/coffeemaker/domain/RecipeBookTest.java
+++ src/test/java/coffeemaker/domain/RecipeBookTest.java
@@ -1,5 +1,20 @@
 package coffeemaker.domain;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class RecipeBookTest {
-    
+    @Test
+    @DisplayName("Default Constructor")
+    public void testDefaultConstructor() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new RecipeBook());
+    }
+
+    // @Test
+    // @DisplayName("getRecipes empty array")
+    // public void testGetRecipes() {
+    //     RecipeBook recipeBook = new RecipeBook();
+    // }
 }
